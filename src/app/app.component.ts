e5a036814260de95929901f54c7fb0bba2cd6c77
Gotham <gotham@greenitive.com>
2022-04-30 22:31:38 +0530

    yes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
-import { ApiService } from './api.service';
-
+//import { ApiService } from './api.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'test';
-  cities:any;
-  constructor(private _api:ApiService){
-    this._api.getData().subscribe((data) => {
-    this.cities=data;
-    })
+  router: any;
+ 
+  constructor(private _router:Router){}
+  toGo(){
+    this._router.navigate(['employee']);
+  }
+  doIt(){
+    this._router.navigate(['/department']);
   }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule} from '@angular/common/http';
+import { DepartmentComponent } from './department/department.component';
+import { ShowDeptComponent } from './department/show-dept/show-dept.component';
+import { AddEditDeptComponent } from './department/add-edit-dept/add-edit-dept.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { ShowEmpComponent } from './employee/show-emp/show-emp.component';
+import { AddEditEmpComponent } from './employee/add-edit-emp/add-edit-emp.component'
+import { SharedService } from './shared.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentComponent,
+    ShowDeptComponent,
+    AddEditDeptComponent,
+    EmployeeComponent,
+    ShowEmpComponent,
+    AddEditEmpComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,ReactiveFormsModule
   ],
-  providers: [],
+  providers: [SharedService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
